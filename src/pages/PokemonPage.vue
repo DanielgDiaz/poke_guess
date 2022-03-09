4134001a9f94878b9da4374af91364ad0bcad9ea
DanielgDiaz <danielgdiaz92@gmail.com>
2022-03-09 19:24:49 +0100

    pokemonPicture component styles :art:

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,22 @@
+<template>
+    
+        <h1>Who's This Pokémon?</h1>
+        
+        <pokemon-picture />
+
+        <pokemon-options />
+</template>
+
+<script>
+import PokemonOptions from '../components/PokemonOptions'
+import PokemonPicture from '../components/PokemonPicture'
+
+export default {
+  components: { PokemonPicture, PokemonOptions },
+
+}
+</script>
+
+<style>
+
+</style>
